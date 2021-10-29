c6b6a512519675e081e16fa8ac7a436f60c80c9a
enebin <46271447+enebin@users.noreply.github.com>
2021-10-29 14:04:24 +0900

    Initial Commit

---
+++ CoffeeShop/CoffeeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopApp.swift
+//  CoffeeShop
+//
+//  Created by 이영빈 on 2021/10/29.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
